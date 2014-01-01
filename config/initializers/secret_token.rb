2ed475d01db351c8e113322f2eb497dadcfe1f08
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2014-01-01 15:40:12 +0000

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sociolog::Application.config.secret_token = 'bf351272751a5ef967e81fd3b709f4323d53b6ab562095708eba8fd8007409ca803bf47e1f11fcb496dcd43760ccd6be1e3dbbc6c1b885064fe4feaf51566f74'
