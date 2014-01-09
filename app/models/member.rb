e958a79100525f60b09d49da827a54bfc0d8b772
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2014-01-09 18:23:01 +0000

    cosmetics.

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,8 +1,12 @@
 class Member < ActiveRecord::Base
   attr_accessible :address, :comment, :email, :forename, :number, :status, :surname, :title
 
-  def name
+  def fullname
     "%s %s %s" % [title, forename, surname]
+  end
+
+  def name 
+    "#{surname}, #{forename}"
   end
 
 end
