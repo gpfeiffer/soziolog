14d0301e2e516b5eb0b7c14442c5d6cdc29b0d92
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2014-01-09 18:23:25 +0000

    define a designated port (3005)

--- config/boot.rb
+++ config/boot.rb
@@ -4,3 +4,12 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+require 'rails/commands/server'
+module Rails
+  class Server
+    def default_options
+      super.merge  :Port => 3005
+    end
+  end
+end
