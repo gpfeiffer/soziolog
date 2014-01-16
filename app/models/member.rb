12eb1d7839366c338490b2366285623a00185178
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2014-01-16 18:56:20 +0000

    added member STATUSES ...
    ... and two convenience methods for printing member information.

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,29 @@
 class Member < ActiveRecord::Base
   attr_accessible :address, :comment, :email, :forename, :number, :status, :surname, :title
 
+  STATUSES = {
+    "O" => "ordinary",
+    "S" => "student",
+    "H" => "honorary",
+    "L" => "life",
+    "R" => "retired",
+    "P" => "???",
+    "AMS" => "reciprocal AMS",
+    "DMV" => "reciprocal DMV",
+    "IMTA" => "reciprocal IMTA",
+    "NZMS" => "reciprocal NZMS",
+    "RSME" => "reciprocal RSME",
+    ## FIXME:  the ones below should all be replaced by one from above.
+    "Life member" => "Life member",
+    "Retiring 2009" => "Retiring 2009",
+    "honorary life member" => "honorary life member", 
+    "Hon 95" => "Hon 95", 
+    "Retired" => "Retired", 
+    "Honorary" => "Honorary",
+  }
+
+  validates :status, :presence => true, :inclusion => STATUSES.keys
+
   def fullname
     "%s %s %s" % [title, forename, surname]
   end
@@ -9,4 +32,11 @@ class Member < ActiveRecord::Base
     "#{surname}, #{forename}"
   end
 
+  def first_last
+    "#{forename} #{surname}"
+  end
+
+  def address_line
+    address.gsub(/\r?\n/, " ")
+  end
 end
