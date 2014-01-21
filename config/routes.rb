37e92a613481d309bb88a5004238f9d17cc6953f
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2014-01-21 09:42:56 +0000

    added user scaffold.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Sociolog::Application.routes.draw do
     put 'users' => 'devise/registrations#update', :as => 'user_registration'
   end
 
-  resource :users
+  resources :users
 
   resources :members
 
