cab12c76b14274df015bb0817c92323fa17bd441
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2014-01-24 20:56:46 +0000

    Non-logged in users cannot do anything.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,8 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
+    # user ||= User.new # guest user (not logged in)
+    return if !user
 
     if user.admin?
       can :manage, :all
