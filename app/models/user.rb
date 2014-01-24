1a7f86d1191b1cc6dff0fe16b43909d41f4e0fa3
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2014-01-24 20:24:54 +0000

    time out in 24h.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :timeoutable, :timeout_in => 24.hours
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
