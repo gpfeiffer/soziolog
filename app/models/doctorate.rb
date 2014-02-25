a858c608b9678516f0b38765f308c7e2da29072d
Goetz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2014-02-25 16:33:32 +0000

    modified doctorates views

--- app/models/doctorate.rb
+++ app/models/doctorate.rb
@@ -1,3 +1,11 @@
 class Doctorate < ActiveRecord::Base
   attr_accessible :advisor, :first, :genealogy_id, :last, :thesis, :title, :university, :year
+
+  def last_first
+    "#{last}, #{first}"
+  end
+
+  def genealogy_url
+    "http://genealogy.math.ndsu.nodak.edu/id.php?id=#{genealogy_id}"
+  end
 end
