098cd840d87859d48ee8abdf7bc0f94d64789025
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2014-02-26 14:08:11 +0000

    compactified doctorates views

--- app/models/doctorate.rb
+++ app/models/doctorate.rb
@@ -1,6 +1,10 @@
 class Doctorate < ActiveRecord::Base
   attr_accessible :advisor, :first, :genealogy_id, :last, :thesis, :title, :university, :year
 
+  def first_last
+    "#{first} #{last}"
+  end
+
   def last_first
     "#{last}, #{first}"
   end
