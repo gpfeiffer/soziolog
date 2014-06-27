e3942bac88fe5a898c2ae1069843e8eef03b30d8
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2014-06-27 20:22:12 +0100

    list funded conferences

--- app/helpers/conferences_helper.rb
+++ app/helpers/conferences_helper.rb
@@ -1,2 +1,14 @@
 module ConferencesHelper
+  def dates_in_words(first_day, last_day)
+    text = first_day.strftime('%b %-d') 
+    if last_day != first_day
+      text += "-"
+      if last_day.month == first_day.month
+        text += last_day.strftime('%-d')
+      else
+        text +=last_day.strftime('%b %-d')
+      end
+    end
+    return text
+  end
 end
