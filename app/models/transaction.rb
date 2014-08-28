f59847665cf781d5feebded60c24f50ea7c7b56a
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2014-08-28 13:54:06 +0100

    subscriptions depend on transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,7 +2,7 @@ class Transaction < ActiveRecord::Base
   attr_accessible :amount, :balance_id, :comment, :date, :text
   
   belongs_to :balance
-  has_many :subscriptions
+  has_many :subscriptions, :dependent => :destroy
 
   has_many :labels, :dependent => :destroy
   has_many :categories, :through => :labels
